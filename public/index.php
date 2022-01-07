e09d758459cf3c8637f15c403e059f3bce981f4c
Joao Chimello <joovitorch@gmail.com>
2022-01-07 14:04:56 -0300

    tela de login

--- public/index.php
+++ public/index.php
@@ -1,31 +1,8 @@
 <?php
 
     require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-    require_once(dirname(__FILE__, 2) . '/src/models/Users.php');
-
-    $user = new User(['name' => 'João', 'email' => 'joao@email.com']);
-    // print_r($user);
-    // $user->email = 'joao_alterado@email.com';
-    // echo '<br><br>';
-    // print_r($user->email);
-    // $user->set('email', "joao_alterado@email.com");
-    // print_r($user->__get('email'));
-
-    // $sql = "SELECT * FROM users";
-    // $result = DataBase::getResultFromQuery($sql);
-
-    // while($row = $result->fetch_assoc()) {
-    //     print_r($row);
-    //     echo "<br>";
-    // }
-
-    print_r($user->get(['id' => 1], 'id, name, email'));
-    echo '<br>';
+    require_once(dirname(__FILE__, 2) . '/src/views/login.php');
     
-    foreach(User::get([], 'name') as $user) {
-        echo $user->name;
-        echo "<br>";
-    }
     
 
 ?>
