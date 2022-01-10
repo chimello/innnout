497363614123d3d5b09a8f02696454769d25f88e
chimello <49577324+chimello@users.noreply.github.com>
2022-01-10 13:56:37 -0300

    Menu Toggle, esconder menu

---
+++ public/assets/js/app.js
@@ -0,0 +1,11 @@
+//função autoinvocada
+(function () {
+    const menuToggle = document.querySelector('.menu-toggle')
+
+    menuToggle.onclick = function (e) {
+        const body = document.querySelector('body')
+        body.classList.toggle('hide-sidebar')
+    }
+})()
+
+
