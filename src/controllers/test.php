7303e703a5bc6f9adf763b24073b68bffd7a87b4
Joao Chimello <joovitorch@gmail.com>
2022-01-10 21:25:46 -0300

    Intervalo de horas trabalhadas, função criada

--- src/controllers/test.php
+++ src/controllers/test.php
@@ -1,17 +1,13 @@
 <?php
 //CONTROLER TEMPORÁRIO!;
 
-$i1 = DateInterval::createFromDateString('9 hours');
-$i2 = DateInterval::createFromDateString('6 hours');
+    loadModel('WorkingHours');
 
-$r1 = sumIntervals($i1, $i2);
-$r2 = subtractIntervals($i1, $i2);
-print_r($r1);
-echo '<br>';
-print_r($r2);
-echo '<br>';
-print_r(getDateFromInterval($r1));
-echo '<br>';
-print_r(getDateFromInterval($r2));
+    $wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
+    
+    echo '<br>';
+    $workedIntervalString = $wh->getWorkedInterval()->format('%H:%I:%S');
+    print_r($workedIntervalString);
+    echo '<br>';
 
 ?>
