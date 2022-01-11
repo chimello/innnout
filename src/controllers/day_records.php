0919d71d51d8fe1a65f1f8e2b8d4ebbaa5206af9
Joao Chimello <joovitorch@gmail.com>
2022-01-10 21:57:40 -0300

    mostrar horários na tela, h. trabalhado e h. saída

--- src/controllers/day_records.php
+++ src/controllers/day_records.php
@@ -7,12 +7,6 @@
     $date = (new Datetime())->getTimestamp();
     $today = strftime('%d de %B de %Y', $date);
 
-    $user = $_SESSION['user'];
-    $records = 
-        WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
-
     loadTemplateView('day_records' ,
-        ['today'=> $today,
-        'records' => $records
-    ]);
+        ['today'=> $today]);
 ?>
